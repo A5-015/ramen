6c5145afbbf2d0287cf5b60c329e9b341e3a28f6
niniack <nishantaswani@nyu.edu>
2021-01-04 14:24:22 +0400

    requirements file + formatting

--- simulation/scripts/run_coracle.py
+++ simulation/scripts/run_coracle.py
@@ -13,12 +13,10 @@ import subprocess
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-import pandas as pd
 from matplotlib.backends.backend_pdf import PdfPages
 
 
-class Visualizer():
-
+class Visualizer:
     def __init__(self):
         self.tables = []
         self.figures = []
@@ -26,10 +24,10 @@ class Visualizer():
     def parse(self, results):
         for key, value in results.items():
 
-            if (key == "table"):
+            if key == "table":
                 self.tables.append(value)
 
-            elif (key == "figures"):
+            elif key == "figures":
                 self.figures.append(value)
 
             if type(value) is dict:
@@ -37,28 +35,30 @@ class Visualizer():
 
     def build_figure(self, fig, ax, raw_figure):
 
-        ax.set(xlabel=raw_figure['x axis']['label'],
-               xlim=(raw_figure['x axis']['start'], raw_figure['x axis']['end']),
-               ylabel=raw_figure['y axis']['label'],
-               ylim=(raw_figure['y axis']['start'], raw_figure['y axis']['end']),
-               title=raw_figure['title'])
+        ax.set(
+            xlabel=raw_figure["x axis"]["label"],
+            xlim=(raw_figure["x axis"]["start"], raw_figure["x axis"]["end"]),
+            ylabel=raw_figure["y axis"]["label"],
+            ylim=(raw_figure["y axis"]["start"], raw_figure["y axis"]["end"]),
+            title=raw_figure["title"],
+        )
 
-        for line in raw_figure['data']:
+        for line in raw_figure["data"]:
             x_points = []
             y_points = []
-            for data_point in line['data']:
-                x_points.append(data_point['x'])
-                y_points.append(data_point['y'])
+            for data_point in line["data"]:
+                x_points.append(data_point["x"])
+                y_points.append(data_point["y"])
             ax.plot(x_points, y_points)
 
     def generate_pdf(self):
 
         with PdfPages("./test.pdf") as export_pdf:
             for table in self.tables:
-                df = pd.DataFrame.from_dict(table, orient='index')
+                df = pd.DataFrame.from_dict(table, orient="index")
                 fig, ax = plt.subplots(figsize=(10, 6))
-                plt.axis('off')
-                pd.plotting.table(ax, df, loc='center')
+                plt.axis("off")
+                pd.plotting.table(ax, df, loc="center")
                 fig.tight_layout()
                 export_pdf.savefig()
                 plt.close()
@@ -77,10 +77,16 @@ def main(arguments):
     default_docker_image = "ghcr.io/a5-015/ramen/ramen-sim:docker"
 
     # Argument parsing
-    parser = argparse.ArgumentParser(description=__doc__,
-                                     formatter_class=argparse.RawDescriptionHelpFormatter)
-    parser.add_argument('-i', '--infile', help="Input file", required=True)
-    parser.add_argument('-d', '--docker', help="Docker image to use", default=default_docker_image)
+    parser = argparse.ArgumentParser(
+        description=__doc__, formatter_class=argparse.RawDescriptionHelpFormatter
+    )
+    parser.add_argument("-i", "--infile", help="Input file", required=True)
+    parser.add_argument(
+        "-d", "--docker", help="Docker image to use", default=default_docker_image
+    )
+    parser.add_argument(
+        "-e", "--experiments", help="Number of experiments to run", default=5
+    )
     args = parser.parse_args(arguments)
 
     with open(args.infile) as file:
@@ -88,22 +94,25 @@ def main(arguments):
         data = json.load(file)
 
         # Encode network simulation data into base64
-        encoded_json = (base64.b64encode(json.dumps(data).encode()).decode())
+        encoded_json = base64.b64encode(json.dumps(data).encode()).decode()
 
         # Construct the command chain
-        command = 'sudo docker run %s /bin/sh -c "python3 base64_to_json.py %s > network.json && ./coracle/coracle_sim.byte -f network.json"' % (args.docker, encoded_json)
+        command = (
+            'sudo docker run %s /bin/sh -c "python3 base64_to_json.py %s > network.json && ./coracle/coracle_sim.byte -f network.json"'
+            % (args.docker, encoded_json)
+        )
 
         # Run the simulation
         simulation_result = subprocess.check_output(command, shell=True, text=True)
 
     # Parse the results
     raw_out = json.loads(simulation_result)
-    raw_res = raw_out['results']
+    raw_res = raw_out["results"]
 
     visualizer = Visualizer()
     visualizer.parse(raw_res)
     visualizer.generate_pdf()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     sys.exit(main(sys.argv[1:]))
--- simulation/scripts/sim_config_json.py
+++ simulation/scripts/sim_config_json.py
@@ -2,7 +2,6 @@
 
 """A simple script that takes network configuration parameters as an infile
 and generates a JSON file for the Coracle Simulator
-
 """
 
 import os
@@ -11,27 +10,29 @@ import argparse
 import json
 from network_generator import Mesh, Star
 
+
 def _link_star_simple(num_nodes):
 
     links = list()
 
-    for i in range(0, num_nodes-1):
+    for i in range(0, num_nodes - 1):
         # full duplex
         temp_out = {}
-        temp_out['start'] = i+1
-        temp_out['end'] = (num_nodes)
-        temp_out['id'] = 2*i+1
-        temp_out['direction'] = "uni"
+        temp_out["start"] = i + 1
+        temp_out["end"] = num_nodes
+        temp_out["id"] = 2 * i + 1
+        temp_out["direction"] = "uni"
         links.append(temp_out)
 
         temp_in = {}
-        temp_in['start'] = (num_nodes)
-        temp_in['end'] = i+1
-        temp_in['id'] = 2*i+2
+        temp_in["start"] = num_nodes
+        temp_in["end"] = i + 1
+        temp_in["id"] = 2 * i + 2
         temp_in["direction"] = "uni"
         links.append(temp_in)
 
     return links
+
 
 def _build_mesh(network, num_nodes, width, length, height):
 
@@ -40,7 +41,9 @@ def _build_mesh(network, num_nodes, width, length, height):
 
     # use network generator API to obtain mesh network
     mesh = Mesh(x=width, y=length, z=height)
-    mesh.add_nodes(n=num_nodes, max_range=350, min_range=300, max_link_budget=8, min_link_budget=4)
+    mesh.add_nodes(
+        n=num_nodes, max_range=350, min_range=300, max_link_budget=8, min_link_budget=4
+    )
 
     mesh.find_neighbors()
     mesh.create_links()
@@ -54,24 +57,24 @@ def _build_mesh(network, num_nodes, width, length, height):
     i = 1
     for key in mesh.network_edges:
         temp = {}
-        temp['start'] = key[0]
-        temp['end'] = key[1]
-        temp['id'] = i
-        temp['direction'] = "bi"
+        temp["start"] = key[0]
+        temp["end"] = key[1]
+        temp["id"] = i
+        temp["direction"] = "bi"
 
-        i+=1
+        i += 1
         links.append(temp)
 
     # store each node as server and append to node list
     for node in mesh.node_list:
         temp = {}
-        temp['type'] = "server"
-        temp['id'] = node
+        temp["type"] = "server"
+        temp["id"] = node
         nodes.append(temp)
 
     #  add node list and link list to network dictionary
-    network['nodes'] = nodes
-    network['links'] = links
+    network["nodes"] = nodes
+    network["links"] = links
 
 
 def _build_star(network, num_nodes, width, length, height):
@@ -81,9 +84,16 @@ def _build_star(network, num_nodes, width, length, height):
 
     star = Star(x=width, y=length, z=height)
     # Nodes have to have a link budget of 1 in the star network
-    star.add_nodes(n=num_nodes, max_range=350, min_range=300, max_link_budget=1, min_link_budget=1)
-    star.set_hub_constraints(n=num_nodes, max_range=250, min_range=200,
-                             max_link_budget=250, min_link_budget=200)
+    star.add_nodes(
+        n=num_nodes, max_range=350, min_range=300, max_link_budget=1, min_link_budget=1
+    )
+    star.set_hub_constraints(
+        n=num_nodes,
+        max_range=250,
+        min_range=200,
+        max_link_budget=250,
+        min_link_budget=200,
+    )
 
     star.find_neighbors(percent_network_coverage=95, draw=False)
     star.place_hubs()
@@ -91,31 +101,31 @@ def _build_star(network, num_nodes, width, length, height):
     star.remove_disconnected_nodes()
 
     # store each link as a bidirectional link and append to link list
-    i=1
+    i = 1
     for key in star.network_edges:
-        temp={}
-        temp['start'] = key[0]
-        temp['end'] = key[1]
-        temp['id'] = i
-        temp['direction'] = "bi"
+        temp = {}
+        temp["start"] = key[0]
+        temp["end"] = key[1]
+        temp["id"] = i
+        temp["direction"] = "bi"
 
-        i+=1
+        i += 1
         links.append(temp)
 
-    server_nodes = list(star.node_list-star.network_hubs)
+    server_nodes = list(star.node_list - star.network_hubs)
 
     # store each non-hub node as server and append to node list
     for node in server_nodes:
         temp = {}
-        temp['type'] = "server"
-        temp['id'] = node
+        temp["type"] = "server"
+        temp["id"] = node
         nodes.append(temp)
-    
+
     # store hub nodeas hub and append to node list
     for node in star.network_hubs:
         temp = {}
-        temp['type'] = "hub"
-        temp['id'] = node
+        temp["type"] = "hub"
+        temp["id"] = node
         nodes.append(temp)
 
     # print(star.network_edges)
@@ -125,23 +135,25 @@ def _build_star(network, num_nodes, width, length, height):
     # star.draw()
 
     #  add node list and link list to network dictionary
-    network['nodes'] = nodes
-    network['links'] = links
+    network["nodes"] = nodes
+    network["links"] = links
+
 
 def create_system_dict(system_dict, args):
 
-    system_dict['termination'] = args.termination
+    system_dict["termination"] = args.termination
 
     # consensus configurations
     consensus = {}
-    consensus['protocol'] = args.protocol
-    consensus['election_timeout_min'] = args.election_min
-    consensus['election_timeout_max'] = args.election_max
-    consensus['heartbeat_interval'] = args.heartbeat
+    consensus["protocol"] = args.protocol
+    consensus["election_timeout_min"] = args.election_min
+    consensus["election_timeout_max"] = args.election_max
+    consensus["heartbeat_interval"] = args.heartbeat
 
-    system_dict['consensus'] = consensus
+    system_dict["consensus"] = consensus
 
     return
+
 
 def configure_network_parameters(system_dict, args):
 
@@ -149,44 +161,46 @@ def configure_network_parameters(system_dict, args):
 
     if (args.topology) == "mesh":
         _build_mesh(network, args.nodes, args.width, args.length, args.height)
-    elif(args.topology) == "star":
+    elif (args.topology) == "star":
         _build_star(network, args.nodes, args.width, args.length, args.height)
     else:
         raise Exception("Invalid topology")
         exit()
 
-    system_dict['network'] = network
+    system_dict["network"] = network
     return
+
 
 def initialize_network_nominal(system_dict, args):
 
     events = list()
 
     init_time = {}
-    init_time['time'] = 0
+    init_time["time"] = 0
 
     links = list()
     nodes = list()
 
-    for i in range (0, len(system_dict['network']['links'])):
+    for i in range(0, len(system_dict["network"]["links"])):
         temp = {}
-        temp['id'] = i+1
-        temp['type'] = 's'
-        temp['active'] = True
+        temp["id"] = i + 1
+        temp["type"] = "s"
+        temp["active"] = True
         links.append(temp)
 
-    for i in range(0, len(system_dict['network']['nodes'])):
+    for i in range(0, len(system_dict["network"]["nodes"])):
         temp = {}
-        temp['id'] = i+1
-        temp['active'] = True
+        temp["id"] = i + 1
+        temp["active"] = True
         nodes.append(temp)
 
-    init_time['links'] = links
-    init_time['nodes'] = nodes
+    init_time["links"] = links
+    init_time["nodes"] = nodes
     events.append(init_time)
-    system_dict['network']['events'] = events
+    system_dict["network"]["events"] = events
 
     # configure everything to start at time 0
+
 
 def write_json(output_arg, system_dict):
     """
@@ -197,31 +211,61 @@ def write_json(output_arg, system_dict):
     :return: None
     """
 
-    json_object = json.dumps(system_dict, indent = 4)
+    json_object = json.dumps(system_dict, indent=4)
 
     output_arg.write(json_object)
 
     return
 
+
 def main(arguments):
 
     # argument parsing
     parser = argparse.ArgumentParser(
-        description=__doc__,
-        formatter_class=argparse.RawDescriptionHelpFormatter)
-    parser.add_argument('-n', '--nodes', help="The approximate number of nodes you desire to have in the network", type=int, required=True)
-    parser.add_argument('--infile', help="Input file", type=argparse.FileType('r'))
-    parser.add_argument('-o', '--outfile', help="Output file",
-                        default=sys.stdout, type=argparse.FileType('w'))
-    parser.add_argument('--termination', help="Termination time of simulation", default=1000)
-    parser.add_argument('--protocol', help="The consensus algorithm", default="raft")
-    parser.add_argument('--election_min', help="The minimum time for the election timeout", default=60)
-    parser.add_argument('--election_max', help="The minimum time for the election timeout", default=300)
-    parser.add_argument('--heartbeat', help="The heartbeat interval", default=30)
-    parser.add_argument('-t', '--topology', help="The network topology", default="mesh")
-    parser.add_argument('--width', help="The x dimension (width) of the virtual simulation space. Default is 5000m", default=1000)
-    parser.add_argument('--length', help="The y dimension (length) of the virtual simulation space. Default is 5000m", default=1000)
-    parser.add_argument('--height', help="The z dimension (height) of the virtual simulation space. Default is 0m", default=0)
+        description=__doc__, formatter_class=argparse.RawDescriptionHelpFormatter
+    )
+    parser.add_argument(
+        "-n",
+        "--nodes",
+        help="The approximate number of nodes you desire to have in the network",
+        type=int,
+        required=True,
+    )
+    parser.add_argument("--infile", help="Input file", type=argparse.FileType("r"))
+    parser.add_argument(
+        "-o",
+        "--outfile",
+        help="Output file",
+        default=sys.stdout,
+        type=argparse.FileType("w"),
+    )
+    parser.add_argument(
+        "--termination", help="Termination time of simulation", default=1000
+    )
+    parser.add_argument("--protocol", help="The consensus algorithm", default="raft")
+    parser.add_argument(
+        "--election_min", help="The minimum time for the election timeout", default=60
+    )
+    parser.add_argument(
+        "--election_max", help="The minimum time for the election timeout", default=300
+    )
+    parser.add_argument("--heartbeat", help="The heartbeat interval", default=30)
+    parser.add_argument("-t", "--topology", help="The network topology", default="mesh")
+    parser.add_argument(
+        "--width",
+        help="The x dimension (width) of the virtual simulation space. Default is 5000m",
+        default=1000,
+    )
+    parser.add_argument(
+        "--length",
+        help="The y dimension (length) of the virtual simulation space. Default is 5000m",
+        default=1000,
+    )
+    parser.add_argument(
+        "--height",
+        help="The z dimension (height) of the virtual simulation space. Default is 0m",
+        default=0,
+    )
 
     args = parser.parse_args(arguments)
 
@@ -238,5 +282,6 @@ def main(arguments):
     # write to JSON
     write_json(args.outfile, system_dict)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     sys.exit(main(sys.argv[1:]))
