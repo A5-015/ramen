f2a20e36a14f1ec65e1352f9561f33b404eef60d
niniack <nishantaswani@nyu.edu>
2021-01-13 14:26:47 +0400

    added packet success ratio plot and updated requirements.txt

--- simulation/scripts/run_coracle.py
+++ simulation/scripts/run_coracle.py
@@ -14,6 +14,7 @@ import numpy as np
 import pandas as pd
 from tqdm import tqdm
 import plotly.graph_objects as go
+from plotly.subplots import make_subplots
 from collections import defaultdict
 
 
@@ -21,6 +22,8 @@ class Visualizer:
     def __init__(self):
         self.raw_results = []
         self.raw_dictionary = defaultdict(list)
+        # Define key
+        self.psr = "packet success ratio"
 
     def _parse(self, result):
         # Store in ordered dictionary for easy retrieval
@@ -35,14 +38,26 @@ class Visualizer:
 
     def _generate_statistics(self):
 
+        # Insert packet success ratio into dictionary
+        self.raw_dictionary[self.psr] = [
+            r / d
+            for r, d in zip(
+                self.raw_dictionary["packets received"],
+                self.raw_dictionary["packets dispatched"],
+            )
+        ]
+
+        # Use dictionary length to inform dataframe size
         num_attributes = len(self.raw_dictionary)
 
+        # Convert dictionary to pandas dataframe
         self.statistics_dataframe = pd.DataFrame(
             np.zeros((num_attributes, 2)),
             columns=["Mean", "Variance"],
             index=self.raw_dictionary.keys(),
         )
 
+        # Calculate the mean and variance values
         for key, value in self.raw_dictionary.items():
             self.statistics_dataframe.loc[key, :][0] = np.mean(value)
             self.statistics_dataframe.loc[key, :][1] = np.var(value)
@@ -56,24 +71,57 @@ class Visualizer:
                     self._parse(value)
 
     def generate_pdf(self):
+        # Calculate mean and variance from raw data
         self._generate_statistics()
-        fig = go.Figure(
-            data=[
-                go.Table(
-                    header=dict(
-                        values=list(["Attributes"])
-                        + list(self.statistics_dataframe.columns)
-                    ),
-                    cells=dict(
-                        values=[
-                            self.statistics_dataframe.index,
-                            self.statistics_dataframe.Mean,
-                            self.statistics_dataframe.Variance,
-                        ]
-                    ),
-                )
-            ]
+
+        # Frame the plots for data visualization
+        fig = make_subplots(
+            rows=3,
+            cols=1,
+            column_widths=[1.0],
+            row_heights=[1.0, 1.0, 1.0],
+            specs=[
+                [{"type": "table"}],
+                [{"type": "scatter"}],
+                [{"type": "scatter"}],
+            ],
+            subplot_titles=("Summary of Statistics", "Packet Success Ratio per Run"),
         )
+
+        # Add table
+        fig.add_trace(
+            go.Table(
+                header=dict(
+                    values=list(["Attributes"])
+                    + list(self.statistics_dataframe.columns)
+                ),
+                cells=dict(
+                    values=[
+                        self.statistics_dataframe.index,
+                        self.statistics_dataframe.Mean,
+                        self.statistics_dataframe.Variance,
+                    ]
+                ),
+            ),
+            row=1,
+            col=1,
+        )
+
+        # Add packet success plot
+        fig.add_trace(
+            go.Scatter(
+                x=[i + 1 for i in range(len(self.raw_dictionary[self.psr]))],
+                y=self.raw_dictionary[self.psr],
+            ),
+            row=2,
+            col=1,
+        )
+        fig.update_xaxes(title_text="Run", tickformat="d", row=2, col=1)
+        fig.update_yaxes(title_text="Packet Success Ratio (%)", row=2, col=1)
+
+        # Configure overall layout
+        fig.update_layout(height=2500, showlegend=False, title_text="Coracle Runs")
+
         fig.show()
 
 
@@ -127,8 +175,6 @@ def main(arguments):
     simulation_results = [
         json.loads(result.strip().decode()) for result in simulation_results
     ]
-
-    # print(simulation_results[0])
 
     # with open(args.infile) as file:
     #     simulation_results = json.load(file)
