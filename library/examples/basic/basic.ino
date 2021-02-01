5fe8b4aea7002145c708156035ad34f2130a5e4c
niniack <nishantaswani@nyu.edu>
2021-02-01 16:49:17 +0400

    fix userend namespace bloat

--- library/examples/basic/basic.ino
+++ library/examples/basic/basic.ino
@@ -5,13 +5,13 @@
 #define MESH_PASSWORD "ramen123*"
 #define MESH_PORT     5555
 
-ramen::server::Server consensus_on_mesh;
+ramenServer consensus_on_mesh_server;
 
 void setup() {
   Serial.begin(115200);
-  consensus_on_mesh.init(MESH_NAME, MESH_PASSWORD, MESH_PORT);
+  consensus_on_mesh_server.init(MESH_NAME, MESH_PASSWORD, MESH_PORT);
 }
 
 void loop() {
-  consensus_on_mesh.update();
+  consensus_on_mesh_server.update();
 }
