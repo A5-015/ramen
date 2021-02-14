8f20bad4e9b78b42dd3e1069aaaf9d6e97678ef4
woswos <barkin@nyu.edu>
2021-02-14 06:41:06 +0000

    expand unit tests

--- library/test/catch_ramen_init.cpp
+++ library/test/catch_ramen_init.cpp
@@ -1,16 +1,67 @@
 #include "catch2/catch.hpp"
 #include "ramen.h"
 
-SCENARIO("Check if ramen initializes correctly") {
-  ramen network;
+SCENARIO("There is only one node in the network") {
+  GIVEN("ramen init") {
+    ramen network;
 
-  // Assign node id for testing
-  network._mesh.setNodeId(12345);
+    // Assign node id for testing
+    uint32_t test_node_id = 12345;
+    network._mesh.setNodeId(test_node_id);
 
-  network.init(MESH_NAME, MESH_PASSWORD, MESH_PORT);
+    network.init(MESH_NAME, MESH_PASSWORD, MESH_PORT);
 
-  REQUIRE(network._state == broth::server::FOLLOWER);
-  REQUIRE(network._term == 0);
-  REQUIRE(network._id == 12345);
-  REQUIRE(network._received_new_append_entry_request == false);
+    REQUIRE(network._state == broth::server::FOLLOWER);
+    REQUIRE(network._term == 0);
+    REQUIRE(network._id == test_node_id);
+    REQUIRE(network._received_new_append_entry_request == false);
+    REQUIRE_FALSE(network._election_alarm == 0);
+
+    // Fast forward mesh time to trigger the existing election alarm
+    network._mesh.setMeshTime(1000000);
+
+    WHEN("State is FOLLOWER") {
+      // Force the state as follower
+      network._state = broth::server::FOLLOWER;
+
+      // Run update once
+      network.update();
+
+      THEN("term should be increased") {
+        REQUIRE(network._term == 1);
+      }
+
+      THEN("node should vote for itself") {
+        REQUIRE(network._voted_for == test_node_id);
+      }
+
+      THEN("node should become candidate") {
+        REQUIRE(network._state == broth::server::CANDIDATE);
+      }
+    }
+
+    WHEN("State is CANDIDATE") {
+      // Force the state as candidate
+      network._state = broth::server::CANDIDATE;
+
+      // Run update once
+      network.update();
+
+      THEN("term should be increased") {
+        REQUIRE(network._term == 1);
+      }
+
+      THEN("node should vote for itself") {
+        REQUIRE(network._voted_for == test_node_id);
+      }
+
+      THEN("node should become candidate again") {
+        REQUIRE(network._state == broth::server::CANDIDATE);
+      }
+
+      THEN("request vote from others") {
+        // Check if _mesh.sendBroadcast() was called and the message itself
+      }
+    }
+  }
 }
