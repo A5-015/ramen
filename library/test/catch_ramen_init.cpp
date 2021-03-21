9b665664c51643b7a90dcc224facd0a283c9a439
woswos <barkin@nyu.edu>
2021-03-21 03:24:25 +0000

    update unit tests to match slowed down raft

--- library/test/catch_ramen_init.cpp
+++ library/test/catch_ramen_init.cpp
@@ -16,6 +16,8 @@ SCENARIO("There is only one node in the network") {
 
     network.init(MESH_NAME, MESH_PASSWORD, MESH_PORT);
 
+    network._mesh.setNodeId(test_node_id);
+
     REQUIRE(network._state == broth::server::FOLLOWER);
     REQUIRE(network._term == 0);
     REQUIRE(network._id == test_node_id);
@@ -25,15 +27,18 @@ SCENARIO("There is only one node in the network") {
     // Fast forward mesh time to trigger the existing election alarm
     network._mesh.setMeshTime(1000000);
 
+    uint32_t term = 0;
+
     WHEN("State is FOLLOWER") {
       // Force the state as follower
       network._state = broth::server::FOLLOWER;
 
-      // Run update once
-      network.update();
+      for(uint32_t i = 0; i < RAFT_TIMER_PERIOD; i++) {
+        network.update();
+      }
 
       THEN("term should be increased") {
-        REQUIRE(network._term == 1);
+        REQUIRE(network._term > 10);
       }
 
       THEN("node should vote for itself") {
@@ -49,11 +54,12 @@ SCENARIO("There is only one node in the network") {
       // Force the state as candidate
       network._state = broth::server::CANDIDATE;
 
-      // Run update once
-      network.update();
+      for(uint32_t i = 0; i < RAFT_TIMER_PERIOD; i++) {
+        network.update();
+      }
 
       THEN("term should be increased") {
-        REQUIRE(network._term == 1);
+        REQUIRE(network._term > 10);
       }
 
       THEN("node should vote for itself") {
