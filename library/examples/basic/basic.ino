6d55b4f1b84d014de90875481518029f74d2af5f
woswos <barkin@nyu.edu>
2021-03-21 20:41:07 +0000

    rename variables in basic example

--- library/examples/basic/basic.ino
+++ library/examples/basic/basic.ino
@@ -5,7 +5,7 @@
 #define MESH_PASSWORD "ramen123*"
 #define MESH_PORT     5555
 
-ramen consensus_on_mesh_server;
+ramen server;
 
 unsigned long previousMillis = 0;
 const long interval = 1000;
@@ -15,10 +15,7 @@ void setup() {
   Serial.begin(115200);
 
   // Initialize ramen
-  consensus_on_mesh_server.init(MESH_NAME,
-                                MESH_PASSWORD,
-                                MESH_PORT,
-                                broth::logger::DEBUG);
+  server.init(MESH_NAME, MESH_PASSWORD, MESH_PORT, broth::logger::DEBUG);
 
   // Set the builtin blue LED as output
   pinMode(2, OUTPUT);
@@ -26,10 +23,10 @@ void setup() {
 }
 
 void loop() {
-  consensus_on_mesh_server.update();
+  server.update();
 
   // If the server is a leader, turn on the blue LED
-  if(consensus_on_mesh_server.getState() == broth::server::LEADER) {
+  if(server.getState() == broth::server::LEADER) {
     digitalWrite(2, LOW);
   } else {
     digitalWrite(2, HIGH);
