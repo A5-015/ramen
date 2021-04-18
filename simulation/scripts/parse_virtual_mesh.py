ab9b06562bc0a9a38062e05413aaa6ba138e5844
woswos <barkin@nyu.edu>
2021-04-18 12:14:36 +0000

    Add simulation output

--- simulation/scripts/parse_virtual_mesh.py
+++ simulation/scripts/parse_virtual_mesh.py
@@ -1,14 +1,22 @@
 import re
 import subprocess
 
-time = 1
-kill_leader_at_time = 0
-n_nodes = 3
-n_logs_to_append = 1
+# time = 60
+# kill_leader_at_time = 10
+# n_nodes = 5
+# n_logs_to_append = 20
 
-command = f"cd ../../library && cmake . && make virtual_esp && ./bin/virtual_esp -t {time} -n {n_nodes} -l {n_logs_to_append} -k {kill_leader_at_time}"
-output = subprocess.check_output(command, shell=True, text=True)
+# command = f"cd ../../library && cmake . && make virtual_esp && ./bin/virtual_esp -t {time} -n {n_nodes} -l {n_logs_to_append} -k {kill_leader_at_time}"
+# output = subprocess.check_output(command, shell=True, text=True)
+
+# f = open("output.txt", "a")
+# f.write(output)
+# f.close()
+
+f = open("output.txt", "r")
+output = f.read()
 
 result = re.findall("(.*(?: @ ).*)", output)
 
-print(result)
+for line in result:
+    print(line)
