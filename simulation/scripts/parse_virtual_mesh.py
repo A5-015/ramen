7b0313b049fc019983f6049d0dd3d76426320038
Barkin Simsek <barkin@nyu.edu>
2021-04-24 11:09:38 +0000

    Finalize virtual mesh parser

--- simulation/scripts/parse_virtual_mesh.py
+++ simulation/scripts/parse_virtual_mesh.py
@@ -7,23 +7,132 @@ import numpy as np
 from matplotlib.ticker import MaxNLocator
 from scipy.stats import sem
 
-time = 1
-one_sec = 1000000
-n_run_for_each = 2
+one_sec = 1000000  # Constant, don't change
+
+n_run_for_each = 10
+time = 60
 
 
 def main():
     n_nodes_test(n_nodes_to_test=[3, 5, 10, 20, 30, 50, 80, 100])
-    heart_beat_test(
-        heart_beat_periods_to_test=[
-            0.25 * one_sec,
-            0.5 * one_sec,
-            0.75 * one_sec,
-            1 * one_sec,
-            1.25 * one_sec,
-            1.5 * one_sec,
-        ],
-        n_nodes=5,
+    # heart_beat_test(
+    #     heart_beat_periods_to_test=[
+    #         0.25 * one_sec,
+    #         0.5 * one_sec,
+    #         0.75 * one_sec,
+    #         1 * one_sec,
+    #         1.25 * one_sec,
+    #         1.5 * one_sec,
+    #     ],
+    #     n_nodes=5,
+    # )
+    # election_timeout_test(
+    #     election_timeouts_to_test=[
+    #         [0.1 * one_sec, 0.2 * one_sec],
+    #         [0.1 * one_sec, 0.3 * one_sec],
+    #         [0.1 * one_sec, 0.4 * one_sec],
+    #         [0.1 * one_sec, 0.5 * one_sec],
+    #         [0.1 * one_sec, 0.6 * one_sec],
+    #         [0.1 * one_sec, 0.7 * one_sec],
+    #         [0.1 * one_sec, 0.8 * one_sec],
+    #         [0.1 * one_sec, 0.9 * one_sec],
+    #         [0.1 * one_sec, 1 * one_sec],
+    #     ],
+    #     n_nodes=5,
+    # )
+
+
+def election_timeout_test(election_timeouts_to_test, n_nodes):
+    average_election_duration_results = []
+    average_election_duration_results_err = []
+    election_win_ratio_results = []
+    election_win_ratio_results_err = []
+
+    for election_timeout in election_timeouts_to_test:
+        n_run_average_election_duration_results = []
+        n_run_election_win_ratio_results = []
+
+        # Run same test n_run_for_each times
+        for n_run in range(n_run_for_each):
+
+            election_timeout_min = int(election_timeout[0])
+            election_timeout_max = int(election_timeout[1])
+
+            print(
+                f"Running the simulation with {n_nodes} node(s) for {time} second(s) with min election timout of {election_timeout_min} us and max election timeout of {election_timeout_max}"
+            )
+
+            average_election_duration, election_win_ratio = run_simulation(
+                time=time,
+                n_nodes=n_nodes,
+                election_min=election_timeout_min,
+                election_max=election_timeout_max,
+            )
+
+            print(f"Average time to win election: {average_election_duration}")
+            print(
+                f"Ratio of elections won those those started: {election_win_ratio}"
+            )
+
+            n_run_average_election_duration_results.append(
+                average_election_duration
+            )
+            n_run_election_win_ratio_results.append(election_win_ratio)
+
+        # Append the average of the runs
+        average_election_duration_results.append(
+            int(avg(n_run_average_election_duration_results))
+        )
+
+        # Calculate standard error
+        average_election_duration_results_err.append(
+            int(
+                np.std(n_run_average_election_duration_results)
+                / np.sqrt(np.size(n_run_average_election_duration_results))
+            )
+        )
+
+        # Append the average of the runs
+        election_win_ratio_results.append(
+            int(avg(n_run_election_win_ratio_results))
+        )
+
+        # Calculate standard error
+        election_win_ratio_results_err.append(
+            int(
+                np.std(n_run_election_win_ratio_results)
+                / np.sqrt(np.size(n_run_election_win_ratio_results))
+            )
+        )
+
+    pprint(
+        {
+            "heart_beat_plot_data": {
+                "election_timeouts_to_test": election_timeouts_to_test,
+                "time": time,
+                "n_nodes": n_nodes,
+                "average_election_duration_results": average_election_duration_results,
+                "average_election_duration_results_err": average_election_duration_results_err,
+                "election_win_ratio_results": election_win_ratio_results,
+                "election_win_ratio_results_err": election_win_ratio_results_err,
+            }
+        }
+    )
+
+    plot(
+        title_1="Average time to win election",
+        title_2="Ratio of elections won those those started",
+        x_1=[x[1] for x in election_timeouts_to_test],
+        y_1=average_election_duration_results,
+        y_1_err=average_election_duration_results_err,
+        x_1_label="Election Timeout Period",
+        y_1_label="Average Election Duration",
+        x_2=[x[1] for x in election_timeouts_to_test],
+        y_2=election_win_ratio_results,
+        y_2_err=election_win_ratio_results_err,
+        x_2_label="Election Timeout Period",
+        y_2_label="Average Election Win Ratio",
+        fig_name="election_timeout.png",
     )
 
 
@@ -205,7 +314,7 @@ def n_nodes_test(n_nodes_to_test):
         y_2_err=election_win_ratio_results_err,
         x_2_label="Number of Nodes",
         y_2_label="Average Election Win Ratio",
-        fig_name="test_n_nodes.png",
+        fig_name="n_nodes.png",
     )
 
 
